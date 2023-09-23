4ff0fc2ac03da92e5ec1583232c4e5a71021bea7
Marcus <marcus@pop-os.localdomain>
2023-09-23 09:58:10 +0100

    UPdate #This should work

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { adapter } from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
+
 
 export default {
-  kit: {
-    adapter: adapter({   
-      // see the 'Deployment configuration' section below
-    
-    })
-  },
+	kit: {
+		adapter: adapter({
+			// see the 'Deployment configuration' section below
+		})
+	}
   preprocess: vitePreprocess()
-  
 };
+
 
